2fab7df1ccd9877c873a2e61f812bc026075d8fd
Rafael Schwan <rafaelschwan@hotmail.com>
2021-07-30 16:42:50 -0300

    Adicionado typeorm e swagger no projeto

--- src/modules/cars/useCases/importCategory/index.ts
+++ src/modules/cars/useCases/importCategory/index.ts
@@ -1,7 +1,11 @@
+import { CategoriesRepository } from '../../repositories/implementations/CategoriesRepository';
 import { ImportCategoryController } from './ImportCategoryController';
 import { ImportCategoryUseCase } from './ImportCategoryUseCase';
 
-const importCategoryUseCase = new ImportCategoryUseCase();
+const importCategoriesRepository = CategoriesRepository.getInstance();
+const importCategoryUseCase = new ImportCategoryUseCase(
+    importCategoriesRepository,
+);
 const importCategoryController = new ImportCategoryController(
     importCategoryUseCase,
 );
